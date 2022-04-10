585c869294e5d57cf8d8ca275b7d6a425ecf61a7
rafalpienkowski <rafal.pienkowski@wp.pl>
2022-04-10 11:54:28 +0200

    Normalize path subdir sign

--- src/Trunk.Logic/Analysis/LinesOfFileCounter.cs
+++ src/Trunk.Logic/Analysis/LinesOfFileCounter.cs
@@ -28,7 +28,7 @@ public class LinesOfFileCounter
         foreach (var file in Directory.GetFiles(path))
         {
             var lines = File.OpenRead(file).CountLines();
-            files.Add(new FileLines(file, lines));
+            files.Add(FileLines.From(file, lines));
 
             foreach (var directory in Directory.GetDirectories(path))
             {
@@ -46,9 +46,11 @@ public class FileLines
     public string Path { get; }
     public long Lines { get; }
 
-    public FileLines(string path, long lines)
+    private FileLines(string path, long lines)
     {
         Path = path;
         Lines = lines;
     }
+
+    public static FileLines From(string path, long lines) => new FileLines(path.Replace(@"\",@"/"), lines);
 }

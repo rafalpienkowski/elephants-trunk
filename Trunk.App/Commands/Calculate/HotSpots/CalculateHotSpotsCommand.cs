41f23d9770f7f5d4b335767f3c2dfcf32467ca94
rafalpienkowski <rafal.pienkowski@wp.pl>
2022-04-20 22:24:39 +0200

    Refactor and UI enhances

--- Trunk.App/Commands/Calculate/HotSpots/CalculateHotSpotsCommand.cs
+++ Trunk.App/Commands/Calculate/HotSpots/CalculateHotSpotsCommand.cs
@@ -23,28 +23,50 @@ public class CalculateHotSpotsCommand : AsyncCommand<CalculateHotSpotsSettings>
 {
     public override async Task<int> ExecuteAsync(CommandContext context, CalculateHotSpotsSettings settings)
     {
-        var loader = new FileSourceControlLogLoader(settings.GitLogFilePath);
-        using var streamReader = await loader.LoadAsync();
-        var revisionParser = new GitRevisionParser();
-        var revisions = await revisionParser.ParseAsync(streamReader);
+        await AnsiConsole.Status().StartAsync("Thinking...", async ctx =>
+        {
+            ctx.Spinner(Spinner.Known.Star);
+            ctx.SpinnerStyle(Style.Parse("green"));
+            
+            ctx.Status("Loading git log file");
+            var loader = new FileSourceControlLogLoader(settings.GitLogFilePath);
+            using var streamReader = await loader.LoadAsync();
+            ctx.Status("Parsing git revisions");
+            var revisionParser = new GitRevisionParser();
+            var revisions = await revisionParser.ParseAsync(streamReader);
 
-        var revisionFrequency = RevisionsAnalyzer.Analyze(revisions);
-        var lines = LinesOfFileCounter.Count(settings.RepositoryPath ?? ".");
+            ctx.Status("Calculating changes");
+            var revisionFrequency = RevisionsAnalyzer.Analyze(revisions);
+            
+            ctx.Status("Counting file lines");
+            var lines = LinesOfFileCounter.Count(settings.RepositoryPath ?? ".");
 
+            ctx.Status("Calculating combined complexity");
+            var combinedComplexities = CalculateCombinedComplexities(revisionFrequency, lines);
+
+            ctx.Status("Saving results");
+            await SaveResultsToCsvAsync(combinedComplexities);
+            
+            PrintTop10ComplexFiles(combinedComplexities.Take(10).ToList());
+        });
+
+        return 0;
+    }
+
+    private static List<CombinedComplexity> CalculateCombinedComplexities(List<EntityFrequency> revisionFrequency, List<FileLines> lines)
+    {
         var combinedComplexities = (from entityFrequencyGroup in revisionFrequency.GroupBy(rf => rf.Entity)
-            where lines.SingleOrDefault(l => l.Path == entityFrequencyGroup.Key) != null
-            select new CombinedComplexity
-            {
-                Entity = entityFrequencyGroup.Key, NumberOfChanges = entityFrequencyGroup.Sum(g => g.RevisionNumber),
-                NumberOfLines = lines.Single(l => l.Path == entityFrequencyGroup.Key).Lines
-            })
+                where lines.SingleOrDefault(l => l.Path == entityFrequencyGroup.Key) != null
+                select new CombinedComplexity
+                {
+                    Entity = entityFrequencyGroup.Key,
+                    NumberOfChanges = entityFrequencyGroup.Sum(g => g.RevisionNumber),
+                    NumberOfLines = lines.Single(l => l.Path == entityFrequencyGroup.Key).Lines
+                })
             .OrderByDescending(cc => cc.NumberOfChanges)
             .ThenByDescending(cc => cc.NumberOfLines)
             .ToList();
-
-        PrintTop10ComplexFiles(combinedComplexities.Take(10).ToList());
-
-        return 0;
+        return combinedComplexities;
     }
 
     private static void PrintTop10ComplexFiles(IEnumerable<CombinedComplexity> combinedComplexities)
@@ -61,12 +83,27 @@ public class CalculateHotSpotsCommand : AsyncCommand<CalculateHotSpotsSettings>
 
         AnsiConsole.Write(table);
     }
+
+    private static async Task SaveResultsToCsvAsync(IEnumerable<CombinedComplexity> combinedComplexities)
+    {
+        const string resultFileName = "hotspots.csv";
+        if (File.Exists(resultFileName))
+        {
+            File.Delete(resultFileName);
+        }
+
+        await using var sw = new StreamWriter(resultFileName);
+        await sw.WriteLineAsync("Entity,Number of changes,Number of lines");
+        foreach (var combinedComplexity in combinedComplexities)
+        {
+            await sw.WriteLineAsync($"{combinedComplexity.Entity},{combinedComplexity.NumberOfChanges},{combinedComplexity.NumberOfLines}");
+        }
+    }
 }
 
 public class CombinedComplexity
 {
     public string Entity { get; set; }
-    
     public long NumberOfChanges { get; set; }
     public long NumberOfLines { get; set; }
 }

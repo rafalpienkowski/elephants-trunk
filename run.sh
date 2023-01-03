4f093044b3aec73002e7d56143ed9fb1b7b29581
Rafal Pienkowski <rafal.pienkowski@m-kopa.com>
2023-01-03 22:12:22 +0100

    Add run script

---
+++ run.sh
@@ -0,0 +1,8 @@
+#!/usr/bin/env bash
+
+Trunk.App dimensions measure lines-of-code .
+Trunk.App dimensions measure frequency-of-changes ./revisions.log
+
+Trunk.App analyze hot-spots ./lines-of-code.csv ./frequencies.csv
+
+Trunk.App visualize d3 hotspots.csv
